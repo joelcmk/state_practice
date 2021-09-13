cb83cd6e13b244333d12e2f85a67146563067825
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-09-13 14:12:33 -0500

    Removed content

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-import React, { useState } from 'react';
-import Movie from './Movie';
+import React from 'react';
 
 const App = () => {
-
-  const [movies, setMovies] = useState([
-    { name: 'Hombre araña', price: 23, id: 44 },
-    { name: 'Hulk', price: 25, id: 795 },
-    { name: 'Los Increibles', price: 48, id: 3470 }
-  ])
-
-  const [price, setPrice] = useState('');
-  const [name, setName] = useState('');
-
-
-  const updateName = (e) => {
-    setName(e.target.value)
-  }
-
-  const updatePrice = (e) => {
-    setPrice(e.target.value)
-  }
-
-  const addMovie = (e) => {
-    e.preventDefault()
-    setMovies(prevMovie => [...prevMovie, { name: name, price: price }]);
-  }
-
-  console.log(price)
   return (
-
-    < div >
-      {movies.map(movie => (
-        <Movie name={movie.name} price={movie.price} key={movie.price} />
-      ))}
-
-      <form onSubmit={addMovie}>
-        <input type='text' name='name' value={name} onChange={updateName} />
-        <input type='text' name='price' value={price} onChange={updatePrice} />
-        <button>Submit</button>
-      </form >
-    </div >
+    <div>
+      <h1>Hello my name is: </h1>
+    </div>
   );
 }
 
