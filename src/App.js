4d6f3bc71e4b9dd57f95f1d0ab4a9f777b3410e9
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-09-16 19:06:49 -0500

    Added input and created handle change function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState({ value: '' });
+  const [movies, setMovies] = useState(
+    [{ name: 'Joaquin', age: 34 },
+    { name: 'Hector', age: 35 },
+    { name: 'Lalo', age: 50 }
+    ]
+  )
+
+  const updateName = (e) => {
+    e.preventDefault();
+    setName('Joel');
+  }
+
+
+  const handleChange = (e) => {
+    setLastName({ value: e.target.value })
+  }
+
+
+
   return (
     <div>
-      <h1>Hello my name is: </h1>
+      <h1>Hello my name is: {name}</h1>
+      <p>My last name is: {lastName.value}</p>
+      {movies.map(re => (
+        <li>{`${re.name} ${re.age}`}</li>
+      ))}
+      <form onSubmit={updateName}>
+        <input name="name" type="text" value={lastName.value} onChange={handleChange} />
+        <button>Click Here</button>
+      </form>
     </div>
   );
 }
